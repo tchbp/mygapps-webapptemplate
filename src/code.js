7da346a179eff32f626c3aee28af21b6ffd69f88
Banpot <tchbp0419@gmail.com>
2021-06-06 22:18:52 +0700

    Added image to GAS Nwsp

--- src/code.js
+++ src/code.js
@@ -1,3 +1,10 @@
+let idnwsplogo = "1xUdhjmLjVwD742QImsa3snzXUDA3J6ny"; //Id Logo .jpg, .png
+let wsData = "1z37VXmy21xy0gIbGlIpHslzrLsctn8pxN9jD6ozP-0I";//Id Sheet Data
+
 function doGet() {
-  
+    let htmlout = HtmlService.createTemplateFromFile('index');
+    htmlout.logo = DriveApp.getFileById(idnwsplogo).getDownloadUrl();
+//เพิ่ม Code
+    return htmlout.evaluate()
+                .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
